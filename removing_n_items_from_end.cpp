73ff5a93db53d6b470fbfdcd6c77c440b350aa6e
awaisalidev13 <awaisali.dev.13@gmail.com>
2026-09-20 18:13:16 +0500

    Added head and tail cases

---
+++ removing_n_items_from_end.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+using namespace std;
+
+
+struct Node{
+    int data;
+    Node* next;
+
+    Node(int value)
+    {
+        data = value;
+        next = nullptr;
+    }
+};
+
+class List{
+    Node* head;
+
+    public:
+
+        List()
+        {
+            head = nullptr;
+        }
+
+        void insert_head(int value)
+        {
+            Node* newNode = new Node(value);
+            if(head == nullptr)
+            {
+                head = newNode;
+            }
+            else
+            {
+                newNode->next = head;
+                head = newNode;
+            }
+        }
+
+        void display()
+        {
+            if(head == nullptr)
+            {
+                cout << "list is empty" << endl;
+            }
+            Node* temp = head;
+
+            while(temp != nullptr)
+            {
+                cout << temp->data << " ";
+                temp = temp->next;
+            }
+            cout << endl;
+        }
+
+        void remove_end(int index)
+        {
+            if(index == 0) return;
+            
+            if(head == nullptr)
+            {
+                cout << "List is empty" << endl;
+            }
+            else
+            {
+                bool is_head = false;
+                bool is_tail = false;
+                
+                Node* temp = head;
+                Node* preTemp = head;
+                
+                int it = 0;
+
+                while(temp != nullptr)
+                {
+                    temp = temp->next;
+                    if(it<=index)
+                    {
+                        it++;
+                    }
+                    else
+                    {
+                        preTemp = preTemp->next;
+                    }
+                }
+
+                if(preTemp == head && it == index)
+                {
+                    head = head->next;
+                    delete preTemp;
+                    preTemp = nullptr;
+                }
+                else if(preTemp->next->next == nullptr  && it == index+1)
+                {
+                    cout << "TAIL EXECUTING" << endl;
+                    Node* rm = preTemp->next;
+                    preTemp->next = nullptr;
+                    delete rm;
+                    rm = nullptr;
+                }
+            }
+        }
+};
+
+
+int main()
+{
+    List L1;
+
+    L1.insert_head(3);
+    L1.insert_head(2);
+    L1.insert_head(1);
+    L1.display();
+
+
+
+    L1.remove_end(3);
+    L1.display();
+    L1.remove_end(1);
+    L1.display();
+    L1.remove_end(1);
+    L1.display();
+    return 0;
+}
