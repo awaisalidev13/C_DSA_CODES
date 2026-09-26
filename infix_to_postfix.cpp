3a0a69c18134822ff65983973621167047d8410b
awaisalidev13 <awaisali.dev.13@gmail.com>
2026-09-26 15:46:12 +0500

    presedence implementation remaining

--- infix_to_postfix.cpp
+++ infix_to_postfix.cpp
@@ -1,23 +1,73 @@
 #include <iostream>
 #include <stack>
-
 using namespace std;
 
-int main()
-{
-    string str;
-    cout << "Enter the infix expression: ";
-    cin >> str;
 
+
+
+
+int pres(char c)
+{
+    if(c == '+' || c == '-')
+    {
+        return 1;
+    }
+    else if(c == '*' || c == '/')
+    {
+        return 2;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+
+void infix_to_postfix(string str)
+{
+    string res = "";
     stack<char> s;
     for(int i=0 ; i<str.length() ; i++)
     {
     
         if(str[i] >= 'A' && str[i] <= 'Z')
         {
-            cout << str[i];
+            res += str[i];
         }
-        
+        if(str[i] == '(')
+        {
+            s.push(str[i]);
+        }
+        if(str[i] == ')')
+        {
+            while(s.top() != '(')
+            {
+                res += s.top();
+                s.pop();
+            }
+            s.pop();
+        }
+        if(str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/')
+        {
+            s.push(str[i]);
+        }
     }
-    
+    while(!s.empty())
+    {
+        res += s.top();
+        s.pop();
+    }
+
+    cout << "Postfix expression: " << res << endl;
+}
+
+
+int main()
+{
+    string str;
+    cout << "Enter the infix expression: ";
+    cin >> str;
+    infix_to_postfix(str);
+    return 0;
+  
 }
