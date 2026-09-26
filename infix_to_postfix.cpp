5e73269876fa04cc12650c725c71976bfac8ee3a
awaisalidev13 <awaisali.dev.13@gmail.com>
2026-09-26 15:12:45 +0500

    basic blueprint completed

---
+++ infix_to_postfix.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cout << "Enter the infix expression: ";
+    cin >> str;
+
+    stack<char> s;
+    for(int i=0 ; i<str.length() ; i++)
+    {
+    
+        if(str[i] >= 'A' && str[i] <= 'Z')
+        {
+            cout << str[i];
+        }
+        
+    }
+    
+}
